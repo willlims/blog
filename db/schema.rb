d4936375e912a55cd9b37d0ad43134b9de6122f3
z my <zmywqh@sina.com>
2016-07-10 16:23:23 +0800

    comments

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160710024229) do
+ActiveRecord::Schema.define(version: 20160710071502) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "title",      limit: 255
@@ -19,6 +19,16 @@ ActiveRecord::Schema.define(version: 20160710024229) do
     t.datetime "created_at",               null: false
     t.datetime "updated_at",               null: false
   end
+
+  create_table "comments", force: :cascade do |t|
+    t.string   "commenter",  limit: 255
+    t.text     "body",       limit: 65535
+    t.integer  "article_id", limit: 4
+    t.datetime "created_at",               null: false
+    t.datetime "updated_at",               null: false
+  end
+
+  add_index "comments", ["article_id"], name: "index_comments_on_article_id", using: :btree
 
   create_table "delayed_jobs", force: :cascade do |t|
     t.integer  "priority",   limit: 4,     default: 0, null: false
@@ -93,4 +103,5 @@ ActiveRecord::Schema.define(version: 20160710024229) do
     t.datetime "updated_at"
   end
 
+  add_foreign_key "comments", "articles"
 end
